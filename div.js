c46d761bef9a3cbbf6e51722348af67527b25608
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-01-09 20:07:11 -0300

    Create div.js

---
+++ div.js
@@ -0,0 +1,15 @@
+const calcular = {
+    '/': (valorAcumulado, numero) => { return parseFloat(Number(valorAcumulado) / Number(numero)); },
+    '*': (valorAcumulado, numero) => { return Number(valorAcumulado) * Number(numero) >= Number.MAX_SAFE_INTEGER ? BigInt(valorAcumulado) * BigInt(numero) : valorAcumulado * numero; }
+};
+
+do {
+    valorAcc = 1;
+    if (!(quantLinhaEntrada = parseInt(gets()))) break;
+
+    while (quantLinhaEntrada--) {
+        [, num, operador] = (gets()).match(/^([0-9]) *([*/])/);
+        valorAcc = calcular[operador](valorAcc, num);
+    }
+    console.log(typeof valorAcc == 'bigint' ? valorAcc : parseInt(valorAcc));
+} while (true);
